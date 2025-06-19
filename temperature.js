8c177b8bbaf96e1b6c40178e021ae47ac0977ec1
AbdulrahmanAlhumid <abdulrahman.alhumid@gmail.com>
2025-06-19 05:04:36 +0300

    temperature converter

---
+++ temperature.js
@@ -0,0 +1,37 @@
+const textBox = document.getElementById("textBox");
+const toF = document.getElementById("toF");
+const toC = document.getElementById("toC");
+const result = document.getElementById("result");
+
+let temp;
+
+
+function convert(){
+    if(toF.checked){
+
+        temp = Number(textBox.value);
+
+        temp = temp * 9 / 5 + 32;
+
+        result.textContent = temp + "°F";
+
+
+
+
+    }
+    else if(toC.checked){
+
+        temp = Number(textBox.value);
+
+        temp = (temp - 32) * (5/9);
+
+        result.textContent = temp + "°C";
+
+       
+
+    }
+    else{
+        result.textContent = "Select a unit";
+    }
+
+}
